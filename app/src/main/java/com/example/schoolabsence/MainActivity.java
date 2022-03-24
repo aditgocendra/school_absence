1a3a984b90c3a6c5b4b2e0ba38b7d2ab13c2eca6
aditgocendra <gocendra123@gmail.com>
2022-03-24 10:42:27 +0700

    update

--- app/src/main/java/com/example/schoolabsence/MainActivity.java
+++ app/src/main/java/com/example/schoolabsence/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.schoolabsence.Utility.GlobalFunction;
+import com.example.schoolabsence.View.Admin.AdministratorMenu;
+import com.example.schoolabsence.View.Admin.CompareDistance;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GlobalFunction.checkWindowSetFlag(this);
+
+        GlobalFunction.updateUI(this, AdministratorMenu.class);
     }
 }
