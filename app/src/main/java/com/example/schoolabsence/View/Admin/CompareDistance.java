9b6d3659a20d1dbd05cc6d5e7dd6b73b68e32622
aditgocendra <gocendra123@gmail.com>
2022-03-30 09:25:27 +0700

    set school location from firebase realtime

--- app/src/main/java/com/example/schoolabsence/View/Admin/CompareDistance.java
+++ app/src/main/java/com/example/schoolabsence/View/Admin/CompareDistance.java
@@ -13,17 +13,19 @@ import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.example.schoolabsence.Model.ModelLocationSchool;
 import com.example.schoolabsence.Utility.Algorithm;
 import com.example.schoolabsence.Utility.GlobalFunction;
+import com.example.schoolabsence.Utility.GlobalVariable;
 import com.example.schoolabsence.databinding.ActivityCompareDistanceBinding;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class CompareDistance extends AppCompatActivity {
 
@@ -48,78 +50,12 @@ public class CompareDistance extends AppCompatActivity {
         locationRequest.setFastestInterval(2000);
 
         listenerComponent();
+        setDataSchoolLocation();
 
     }
 
     private void listenerComponent(){
         binding.backBtn.setOnClickListener(view -> finish());
-
-        binding.setLocationBtnSchool.setOnClickListener(view -> {
-            binding.progressCircular.setVisibility(View.VISIBLE);
-            binding.setLocationBtnSchool.setEnabled(false);
-
-            String latSchoolText = binding.schoolLatEdt.getText().toString();
-            String longSchoolText = binding.schoolLongEdt.getText().toString();
-            String address = binding.schoolLocationEdt.getText().toString();
-
-            if (latSchoolText.isEmpty() && longSchoolText.isEmpty() && !address.isEmpty()){
-                LatLng schoolLatLang = GlobalFunction.getLocationFromAddress(CompareDistance.this, address);
-                if (schoolLatLang != null){
-                    latitudeSchool = schoolLatLang.latitude;
-                    longitudeSchool = schoolLatLang.longitude;
-                    binding.schoolLatEdt.setText(String.valueOf(latitudeSchool));
-                    binding.schoolLongEdt.setText(String.valueOf(longitudeSchool));
-                }else {
-                    Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show();
-                }
-            }else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ActivityCompat.checkSelfPermission(CompareDistance.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        if (GlobalFunction.isGPSEnabled(CompareDistance.this)) {
-                            LocationServices.getFusedLocationProviderClient(CompareDistance.this).requestLocationUpdates(locationRequest, new LocationCallback() {
-                                @Override
-                                public void onLocationResult(@NonNull LocationResult locationResult) {
-                                    super.onLocationResult(locationResult);
-                                    LocationServices.getFusedLocationProviderClient(CompareDistance.this).removeLocationUpdates(this);
-                                    if (locationResult.getLocations().size() > 0){
-                                        int index = locationResult.getLocations().size() - 1;
-
-
-                                        latitudeSchool = locationResult.getLocations().get(index).getLatitude();
-                                        longitudeSchool = locationResult.getLocations().get(index).getLongitude();
-
-
-                                        Geocoder geocoder = new Geocoder(CompareDistance.this, Locale.getDefault());
-                                        try {
-                                            addressList = geocoder.getFromLocation(latitudeSchool, longitudeSchool,1);
-                                            binding.schoolLocationEdt.setText(addressList.get(0).getAddressLine(0));
-                                            binding.schoolLatEdt.setText(String.valueOf(latitudeSchool));
-                                            binding.schoolLongEdt.setText(String.valueOf(longitudeSchool));
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }else {
-                                        Toast.makeText(CompareDistance.this, "Find location failed", Toast.LENGTH_SHORT).show();
-                                    }
-                                    binding.progressCircular.setVisibility(View.GONE);
-                                    binding.setLocationBtnSchool.setEnabled(true);
-                                }
-                            }, Looper.getMainLooper());
-
-                        } else {
-                            GlobalFunction.turnOnGPS(locationRequest, CompareDistance.this);
-                            binding.progressCircular.setVisibility(View.GONE);
-                            binding.setLocationBtnSchool.setEnabled(true);
-                        }
-                    } else {
-                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1005);
-                        binding.progressCircular.setVisibility(View.GONE);
-                        binding.setLocationBtnSchool.setEnabled(true);
-                    }
-                }
-            }
-
-        });
 
         binding.setLocationBtnYour.setOnClickListener(view -> {
             binding.progressCircular.setVisibility(View.VISIBLE);
@@ -199,21 +135,6 @@ public class CompareDistance extends AppCompatActivity {
             }
         });
 
-        binding.clearDataBtn.setOnClickListener(view -> {
-            binding.schoolLatEdt.setText("");
-            binding.schoolLongEdt.setText("");
-            binding.schoolLocationEdt.setText("");
-            binding.yourLatEdt.setText("");
-            binding.yourLongEdt.setText("");
-            binding.yourLocationEdt.setText("");
-            latitudeSchool = 0;
-            longitudeSchool = 0;
-            latitudeCurrent = 0;
-            longitudeCurrent = 0;
-            binding.resultDistanceHaversine.setText("-");
-            binding.resultDistanceEuclidean.setText("-");
-        });
-
     }
 
     private void compareAlgorithm(){
@@ -238,6 +159,23 @@ public class CompareDistance extends AppCompatActivity {
         binding.progressCircular.setVisibility(View.GONE);
         binding.compareBtn.setEnabled(true);
 
+    }
+
+    private void setDataSchoolLocation(){
+        GlobalVariable.reference.child("location_school").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                ModelLocationSchool modelLocationSchool = task.getResult().getValue(ModelLocationSchool.class);
+                if (modelLocationSchool != null){
+                    binding.schoolLocationEdt.setText(modelLocationSchool.getAddress());
+                    binding.schoolLatEdt.setText(String.valueOf(modelLocationSchool.getMedian_lat()));
+                    binding.schoolLongEdt.setText(String.valueOf(modelLocationSchool.getMedian_long()));
+                    latitudeSchool = modelLocationSchool.getMedian_lat();
+                    longitudeSchool = modelLocationSchool.getMedian_long();
+                }
+            }else{
+                Toast.makeText(CompareDistance.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
