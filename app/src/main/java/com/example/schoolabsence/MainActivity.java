bb38018ddbcbb2d671c08364b11daedf891cbd92
aditgocendra <gocendra123@gmail.com>
2022-03-30 09:25:48 +0700

    routes update

--- app/src/main/java/com/example/schoolabsence/MainActivity.java
+++ app/src/main/java/com/example/schoolabsence/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.schoolabsence;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.os.Handler;
 import com.example.schoolabsence.Utility.GlobalFunction;
-import com.example.schoolabsence.View.Admin.AdministratorMenu;
-import com.example.schoolabsence.View.Admin.CompareDistance;
+import com.example.schoolabsence.Utility.GlobalVariable;
+import com.example.schoolabsence.View.Authentication.Auth;
+import com.example.schoolabsence.View.HomeApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         GlobalFunction.checkWindowSetFlag(this);
 
-        GlobalFunction.updateUI(this, AdministratorMenu.class);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null){
+            GlobalVariable.uidCurrent = user.getUid();
+            Handler handler = new Handler();
+            handler.postDelayed(() -> GlobalFunction.updateUI(MainActivity.this, HomeApp.class), 300);
+        }else {
+            GlobalFunction.updateUI(this, Auth.class);
+        }
     }
 }
